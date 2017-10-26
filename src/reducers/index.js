4c61f8b40d9011b5d6b1c51c00c5244f96ace2fb
Daniel Spiers <danllatas@gmail.com>
2017-10-26 16:45:27 +0200

    Add selectedSubreddit and postsBySubreddit reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import context from './context';
+import { selectedSubreddit, postsBySubreddit }  from './reddit';
 
 const reducer = combineReducers({
-    context
+    context,
+    selectedSubreddit,
+    postsBySubreddit
 });
 
 export default reducer
